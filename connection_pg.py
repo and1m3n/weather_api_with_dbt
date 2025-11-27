4f78d4cad8038db24286e97049188f611789062c
and1m3n <and1m3n@mail.ru>
2025-11-27 14:48:11 +0000

    init: project setup with postgres and weather api

---
+++ connection_pg.py
@@ -0,0 +1,78 @@
+import psycopg2
+import os
+from api_request import mock_feth_data
+from dotenv import load_dotenv
+
+def connect_to_bd():
+    print("Connecting to the PostgresSQL db")
+    try:
+        load_dotenv()
+        conn = psycopg2.connect(
+            host=os.getenv("host"),
+            port=os.getenv("port"),
+            dbname=os.getenv('dbname'),
+            user=os.getenv('dbuser'),
+            password=os.getenv('dbpassword')
+        )
+        return conn
+    except psycopg2.Error as e:
+        print('error conection to DB :{e}')
+
+def crate_table(conn):
+    print('Creating table if not exists...')
+    try:
+        cursor=conn.cursor()
+        cursor.execute("""
+        CREATE SCHEMA IF NOT EXISTS dev;
+        CREATE TABLE IF NOT EXISTS dev.raw_weather_data(
+            id SERIAL PRIMARY KEY,
+            city TEXT,
+            temperature FLOAT,
+            weather_desc TEXT,
+            wind_speed FLOAT,
+            time TIMESTAMP,
+            inserted_at TIMESTAMP DEFAULT NOW()
+        );
+        """ )
+        conn.commit()
+        print('Table was created')
+    except psycopg2.Error as e:
+        print(f'Failed to create: {e}')
+
+def insert_records(conn,data):
+    print('Insert data to bd')
+    try:
+        weather = data['current']
+        location= data['location']
+        cursor=conn.cursor()
+        cursor.execute("""
+            INSERT INTO dev.raw_weather_data(
+                    city,
+                    temperature,
+                    weather_desc,
+                    wind_speed,
+                    time) VALUES(%s,%s,%s,%s,%s)
+        """,(
+        location['name'],
+        weather['temperature'],
+        weather['weather_descriptions'][0],
+        weather['wind_speed'],
+        location['localtime']
+        ))
+        conn.commit()
+        print('data successfuly iserted')
+    except psycopg2.Error as e:
+        print(f'Error inserting data into db: {e}')
+
+def main():
+    try:
+        data = mock_feth_data()
+        conn = connect_to_bd()
+        crate_table(conn)
+        insert_records(conn,data)
+    except Exception as e:
+        print(f'error as e:{e}')
+    finally:
+        if  'conn' in locals():
+            conn.close()
+            print('Database connetion closed.')
